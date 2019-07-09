019f2ef71b83b06f5a2a9b5828fba0d6480f4420
Janson <princecx@126.com>
2019-07-09 17:13:11 +0800

    v0.3 restful API design

--- back-end/config.py
+++ back-end/config.py
@@ -1,9 +1,20 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+# basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
 
 class Config(object):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', prefix + os.path.join(basedir, 'app.db'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
